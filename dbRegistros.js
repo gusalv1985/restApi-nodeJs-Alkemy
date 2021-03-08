6d7e77e565cfa9dd0542d86ef9498d91ec3d0d64
Gustavo <alvarezmarcelo1985@gmail.com>
2021-03-08 10:18:30 -0300

    add put and delete

--- dbRegistros.js
+++ dbRegistros.js
@@ -1,49 +1,82 @@
-var config = require('./dbConfig'); 
+var config = require('./dbConfig');
 const sql = require('mssql');
 
-async function getRegistros(){
-    try{
-        let pool = await  sql.connect(config);
+async function getRegistros() {
+    try {
+        let pool = await sql.connect(config);
         let registros = await pool.request().query('select * from registro');
         return registros.recordsets;
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
     }
 }
 
-async function getRegistro(id){
-    try{
-        let pool = await  sql.connect(config);
+async function getRegistro(id) {
+    try {
+        let pool = await sql.connect(config);
         let registros = await pool.request()
-            .input('id', sql.Int , id)
+            .input('id', sql.Int, id)
             .query('select * from registro where id = @id');
         return registros.recordsets;
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
     }
 }
 
-async function postRegistros(reg){
-    try{
-        let pool = await  sql.connect(config);
+async function postRegistros(reg) {
+    try {
+        let pool = await sql.connect(config);
         let registros = await pool.request()
             .input('concepto', sql.VarChar, reg.concepto)
-            .input('monto',sql.Int, reg.monto)
+            .input('monto', sql.Int, reg.monto)
             .input('fecha', sql.Date, reg.fecha)
             .input('tipo', sql.VarChar, reg.tipo)
             .query('INSERT INTO registro (concepto,monto,fecha,tipo) VALUES (@concepto,@monto,@fecha,@tipo)');
         return registros.recordsets;
     }
-    catch(err){
+    catch (err) {
+        console.log(err.message);
+    }
+}
+
+async function putRegistro(reg, id) {
+    try {
+        let pool = await sql.connect(config);
+        let registros = await pool.request()
+            .input('id', sql.Int, id)
+            .input('concepto', sql.VarChar, reg.concepto)
+            .input('monto', sql.Int, reg.monto)
+            .input('fecha', sql.Date, reg.fecha)
+            .input('tipo', sql.VarChar, reg.tipo)
+            .query('UPDATE registro SET concepto = @concepto, monto = @monto, fecha = @fecha, tipo = @tipo WHERE id = @id;');
+        return registros.recordsets;
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+}
+
+async function deleteRegistro(id) {
+    try {
+        let pool = await sql.connect(config);
+        let registros = await pool.request()
+            .input('id', sql.Int, id)
+            .query('DELETE FROM registro WHERE id = @id;');
+        return registros.recordsets;
+    }
+    catch (err) {
         console.log(err.message);
     }
 }
 
 
+
 module.exports = {
-    getRegistros : getRegistros,
-    getRegistro : getRegistro,
-    postRegistros : postRegistros
+    getRegistros: getRegistros,
+    getRegistro: getRegistro,
+    postRegistros: postRegistros,
+    putRegistro: putRegistro,
+    deleteRegistro: deleteRegistro
 }
--- rutas.js
+++ rutas.js
@@ -4,24 +4,34 @@ const dbOperations = require('./dbRegistros')
 
 
 router.route("/")
-                .get((req,res)=>{
-                    dbOperations.getRegistros().then(results => {
-                        res.json(results[0]);
-                    })
-                })
-                .post((req,res)=>{
-                    let reg = req.body
-                    dbOperations.postRegistros(reg).then(results => {
-                        res.status(200).json(results)
-                    })
-                })
+    .get((req, res) => {
+        dbOperations.getRegistros().then(results => {
+            res.json(results[0]);
+        })
+    })
+    .post((req, res) => {
+        let reg = req.body
+        dbOperations.postRegistros(reg).then(results => {
+            res.status(200).json(results)
+        })
+    })
 
 router.route("/:id")
-                .get((req,res)=>{
-                    dbOperations.getRegistro(req.params.id).then(results => {
-                        res.json(results[0]);
-                    })
-                })
+    .get((req, res) => {
+        dbOperations.getRegistro(req.params.id).then(results => {
+            res.json(results[0]);
+        })
+    })
+    .put((req, res) => {
+        dbOperations.putRegistro(req.body, req.params.id).then(results => {
+            res.status(200).json(results)
+        })
+    })
+    .delete((req, res) => {
+        dbOperations.deleteRegistro(req.params.id).then(results => {
+            res.status(200).json(results)
+        })
+    })
 
 
 module.exports = router
